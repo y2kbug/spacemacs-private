b1e2edc8b879e541263e9763028a51ec66768c3f
Turing <xiongximanyou98@gmail.com>
2017-05-05 17:33:31 +0800

    modify the spacemacs theme

--- init.el
+++ init.el
@@ -37,7 +37,6 @@ values."
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      ivy
-     helm
      auto-completion
      better-defaults
      emacs-lisp
@@ -132,9 +131,12 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(monokai
+   dotspacemacs-themes '(sanityinc-solarized-light
+                         sanityinc-tomorrow-eighties
                          spacemacs-dark
-                         spacemacs-light)
+                         monokai
+                         spacemacs-light
+                         )
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
